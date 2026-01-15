4514bbef7279de7a0273271da4a400c0f776c492
webioann <webioann@gmail.com>
2026-01-15 23:35:46 +0200

    Auth screen design was created

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,10 +1,30 @@
-import SafeAreaScreen from '@/components/SafeAreaScreen'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import SafeAreaScreen from '@/components/SafeAreaScreen';
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Auth_Screen = () => {
     return (
         <SafeAreaScreen>
+            <Image 
+                source={require('@/assets/images/auth-image.png')} 
+                style={styles.auth_image}
+                resizeMode='contain'
+            />
+            {/* oAuth Google authentication button */}
+            <TouchableOpacity style={[styles.auth_button, { borderColor: '#bbbbbb'}]}>
+                <Image source={require('@/assets/images/react-logo.png')} style={styles.btn_image} />
+                <Text style={{ fontWeight: 'bold', color: 'grey', fontSize: 16 }}>
+                    Continue with Google
+                </Text>
+            </TouchableOpacity>
+            {/* oAuth Github authentication button */}
+            <TouchableOpacity style={[styles.auth_button, { borderColor: '#bbbbbb'}]}>
+                <Image source={require('@/assets/images/react-logo.png')} style={styles.btn_image} />
+                <Text style={{ fontWeight: 'bold', color: 'grey', fontSize: 16 }}>
+                    Continue with Github
+                </Text>
+            </TouchableOpacity>
+
             <View style={styles.auth_screen}>
                 <Text>index vvvvvv</Text>
             </View>
@@ -19,5 +39,27 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    auth_image: {
+        width: '100%',
+        height: 200,
+        marginTop: 30
+    },
+    auth_button: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+        borderWidth: 2,
+        marginTop: 10,
+        padding: 10,
+        width: '80%',
+        alignSelf: 'center',
+    },
+    btn_image: {
+        width: 30,
+        height: 30,
+        marginRight: 10,
     }
 })
